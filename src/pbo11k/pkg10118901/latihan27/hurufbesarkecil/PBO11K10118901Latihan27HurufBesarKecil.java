e3926585e3496c021f7fd40069d6f8c09d0ebeef
Gonior <43252085+Gonior@users.noreply.github.com>
2019-10-05 01:48:31 +0700

    Dibuat : Sabtu,5 Oktober 2019

---
+++ src/pbo11k/pkg10118901/latihan27/hurufbesarkecil/PBO11K10118901Latihan27HurufBesarKecil.java
@@ -0,0 +1,30 @@
+package pbo11k.pkg10118901.latihan27.hurufbesarkecil;
+import java.util.*;
+/**
+ *
+ * @author ACER
+ * Nama     : Firman Alfinas
+ * Kelas    : IF 11-K
+ * Nim      : 10118904
+ * Deskripsi Program : Program ini berisi program untuk mengganti kalimat ke ukuran besar dan kecil
+ */
+public class PBO11K10118901Latihan27HurufBesarKecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Masukan Kalimat : ");
+        Scanner input = new Scanner(System.in);
+        
+        String str = input.nextLine(); 
+        String strU = str.toUpperCase();
+        String strL = str.toLowerCase();
+        
+        System.out.println("====== Hasil Formating ======");
+        System.out.println("Hasil Huruf Besar : " + strU);
+        System.out.println("Hasil Huruf Kecil : " + strL);
+    }
+    
+}
